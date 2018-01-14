867390688df86964f49453da9e66078a21b9e267
Максим Балахонов <maksimbalahonov@MacBook-Pro-Maksim.local>
2018-01-14 12:52:46 -0800

    Initial Commit

---
+++ ReBuilding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReBuilding
+//
+//  Created by Максим Балахонов on 14.01.18.
+//  Copyright © 2018 Максим Балахонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
